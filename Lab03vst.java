f37a1017ff603c5fd657f6bfe44e9e0c17264c31
14276 <analiese.drupka@vtsd.com>
2022-09-16 07:58:35 -0400

    t

--- Lab03vst.java
+++ Lab03vst.java
@@ -9,7 +9,10 @@ public class Lab03vst
 	public static void main(String[] args)
 	{
       System.out.println("Lab03, 80 Point Version\n");
-      
+      int seconds=10000;
+	  int hours=seconds/3600;
+	  System.out.println("Hours; "+hours);
+	  int remainder = (seconds%3600);
 	}
 }
 
