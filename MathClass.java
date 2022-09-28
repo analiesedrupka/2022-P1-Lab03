b861883be511145c1d3b48de7a97b318f28f1dce
14276 <analiese.drupka@vtsd.com>
2022-09-28 07:53:31 -0400

    t

---
+++ MathClass.java
@@ -0,0 +1,17 @@
+public class MathClass {
+    public static void main (String args[]) {
+        int n1 = 24;
+        int n2 = 7;
+        double n3 = 15;
+        double n4 = 8;
+
+        double sqrtN1 = Math.sqrt(n1);
+        System.out.println(Math.ceil(sqrtN1));
+
+    }
+
+
+
+
+
+}
